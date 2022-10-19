40f08d0c2716fdae0a94e8c5a947f7c744532f5a
EC2 Default User <ec2-user@ip-172-31-39-55.ap-northeast-1.compute.internal>
2022-10-19 06:47:10 +0000

    [Add]商品実装(顧客)

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,18 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all.page(params[:page])
   end
 
   def show
+    @cart_item = CartItem.new
+    @item = Item.find(params[:id])
   end
 
   def new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_image, :item_name, :item_body, :genre_id, :none_taxed_price, :sale_status)
   end
 end
