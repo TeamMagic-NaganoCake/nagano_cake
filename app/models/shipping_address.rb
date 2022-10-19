fa7c9bec5bd5c853c99e082c838dccdc45588881
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-19 08:25:57 +0000

    [add]cusotmers_shipping_address

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,8 +1,8 @@
 class ShippingAddress < ApplicationRecord
 
   validates :postcode, presence: :ture
-  validates :address, presence: :ture
-  validates :address_name, presence: :true
+  validates :address, presence: :ture, format: {with:/\A\d{7}\z/, message: "７桁で入力してください"}
+  validates :address_name, presence: :true, format: {with:/\A[一-龥ぁ-ん]/,message: "漢字・ひらがなのどちらかで入力して下さい"}
 
 
   belongs_to :customer
