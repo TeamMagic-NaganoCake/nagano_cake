c69941864a10051980cb78a5ec07a1bec43749e9
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-19 04:26:52 +0000

    [add]customers_shipping_addresses

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,11 +1,18 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
+    @address = ShippingAddress.new
   end
 
   def edit
   end
 
   def create
+    @address = ShippingAddress.new(shipping_addresses_params)
+    if @address.save
+      redirect_to shipping_addreses_path, notice: "配送先が追加されました。"
+    else
+      render :index
+    end
   end
 
   def update
@@ -13,4 +20,10 @@ class Public::ShippingAddressesController < ApplicationController
 
   def destroy
   end
+
+  private
+    def shipping_addresses_params
+      params.reqire(:shippig_addresses).permit(:postcode, :address, :address_name)
+    end
+
 end
