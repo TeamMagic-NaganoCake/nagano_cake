a114957c81e3601d059262b53ab14fdeae6e9286
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-10-20 06:57:11 +0000

    firstcommit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(
-    email: 'magic@team.com',
-    password: 'teammagic',
+    email: ENV['SECRET_EMAIL'],
+    password: ENV['SECRET_PASSWORD']
 )
