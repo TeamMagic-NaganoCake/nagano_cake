28d87fed6e91c0ed2178cd282c92778693613d43
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-10-20 05:23:02 +0000

    fixed first

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -51,12 +51,12 @@ class Admin::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-   def after_sign_up_path_for(resource)
-     admin_items_path(resource)
-   end
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
   # The path used after sign up for inactive accounts.
-   def after_inactive_sign_up_path_for(resource)
-     admin_root_path(resource)
-   end
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
