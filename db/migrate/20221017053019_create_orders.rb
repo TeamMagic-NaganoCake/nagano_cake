735acb7a41ff8cd36d6cf78860da3f836605cfdd
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-21 06:09:39 +0000

    [add]postage_default_800

--- db/migrate/20221017053019_create_orders.rb
+++ db/migrate/20221017053019_create_orders.rb
@@ -1,15 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.integer :customer_id,           null: false 
+      t.integer :customer_id,           null: false
       t.integer :order_status,          null: false, default: 0
-      t.integer :postage,               null: false
+      t.integer :postage,               null: false, default: 800
       t.integer :billing_price,         null: false
       t.string :order_postcode,         null: false
       t.string :order_address,          null: false
       t.string :order_address_name,     null: false
       t.integer :payment,               null: false, default: 0
-      
+
       t.timestamps
     end
   end
