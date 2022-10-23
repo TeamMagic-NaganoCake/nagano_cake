7f639c4a26c0006f4279f9d9da83f4b3824d6aba
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-10-23 14:01:14 +0000

    [Fix]管理者側と顧客側のヘッダーの区別

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,4 +1,5 @@
 class Admin::SearchesController < ApplicationController
+  layout "admin_application"
   def search
    @search_name = params[:content]
    @items = Kaminari.paginate_array(Item.search_for(@search_name)).page(params[:page])
