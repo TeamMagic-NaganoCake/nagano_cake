ae7940d25ef4a23d9b4bcf8bd90bd3023417593c
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-23 06:35:01 +0000

    [fix]admin_order_sort

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def index
     @customer = Customer.find(params[:id])
-    @orders = @customer.orders.page(params[:page])
+    @orders = @customer.orders.page(params[:page]).order(created_at: :desc)
   end
   def show
     @order = Order.find(params[:id])
@@ -10,8 +10,9 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-     order.update(order_params)
-    if  order.order_status = 1
+
+    order.update(order_params)
+    if  order.order_status == "payment_comfirmation"
       order.order_items.update_all(manufacture_status: 1)
     end
     redirect_to admin_order_path(order)
