e8b7b162ad2ac3c171a2d7758a2d8a149aae2ae5
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-23 04:07:29 +0000

    [fix]admin_order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,6 +10,10 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
+    if @order.order_status == 1
+      @order.order_items.manufacture_status = 1
+      redirect_to admin_order_item_path(@order.order_items)
+    end
     @order.update(order_params)
     redirect_to admin_order_path(@order)
   end
