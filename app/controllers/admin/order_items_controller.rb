a41e18b38c250739702faffef2ec34b28a1c4955
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-10-25 05:42:02 +0000

    注文・製作ステータスの非同期化

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,10 +1,10 @@
 class Admin::OrderItemsController < ApplicationController
   layout "admin_application"
   def update
-    order_item = OrderItem.find(params[:id])
-    @order = order_item.order
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
     @order_items = @order.order_items.all
-    order_item.update(order_item_params)
+    @order_item.update(order_item_params)
     if @order_items.find_by(manufacture_status: "manufacturing")
       @order.update(order_status: "producting")
     elsif @order_items.where(manufacture_status: "manufactured").count == @order_items.count
