e3f9e16edfecba8a36db1be36fbe46e8fdc31dc5
Your Name <you@example.com>
2022-10-27 02:32:48 +0000

    [Layout]修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,9 +4,9 @@ class Public::SearchesController < ApplicationController
     model = params[:model]
     @search_name = params[:content]
     if model == "genre"
-      @items = Kaminari.paginate_array(Genre.search_for(@search_name)).page(params[:page])
+      @items = Kaminari.paginate_array(Genre.search_for(@search_name)).page(params[:page]).per(8)
     elsif model == "item"
-       @items = Kaminari.paginate_array(Item.search_for(@search_name)).page(params[:page])
+       @items = Kaminari.paginate_array(Item.search_for(@search_name)).page(params[:page]).per(8)
     else
       @item = Item.all
     end
